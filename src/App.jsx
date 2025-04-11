a8049d7965da0d825b7a1574b402995c473a833d
Maurice Drew <drewm1987@gmail.com>
2025-04-10 21:28:35 -0500

    stuck at NavBar #6

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
 import { useState } from 'react'
-
 import './App.css'
+import {Routes, Route, Link} from "react-router-dom"
+import {Blue} from 'react-router-dom'
+import {Red} from 'react-router-dom'
+import {Home} from "react-router-dom"
 
 function App() {
 
 
   return (
-    <>
-     
-    </>
+   
+     <div id="container">
+      
+      <div id="navbar">
+        <Link to='/blue'>Blue</Link>
+        <Link to='/red'>Red</Link>
+        <Link to='/'></Link>
+      </div>
+      <div id="main-section">
+        <Routes>
+          <Route path="/blue"
+          element={Blue}
+          />
+          <Route path="/red"
+          element={Red}
+          />
+            <Route path="/"/>
+          
+        </Routes>
+      </div>
+    </div>
+   
   )
 }
 
